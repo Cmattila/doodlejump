97d74f4a0c7419ccbff3a6fea3acb7bc65422dac
cuiyongxiu <cuiyongxiu@gmail.com>
2010-09-01 10:39:28 +0000

    增加Flash，实现BlazeDS通信

---
+++ server/src/tk/doodlejump/Hello.java
@@ -0,0 +1,14 @@
+package tk.doodlejump;
+
+public class Hello {
+	public String hello(String name){  
+        System.out.println("flex调用我了，真好~~~~");  
+        return "hello "+name;  
+    }
+	
+	public String test(String name, String sjif){
+		System.out.println("flex调用我了，真好~~~~");
+		return "hello "+name;
+	}
+}
+
