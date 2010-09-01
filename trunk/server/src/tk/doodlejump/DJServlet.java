7cb7bf3bd15fe092e2440a8087e812c2887f2cf8
cuiyongxiu@gmail.com <cuiyongxiu@gmail.com@da5f8fa4-cf6d-97ba-689b-c456e68314ca>
2010-09-01 04:14:18 +0000

    Add the index.jsp

--- trunk/server/src/tk/doodlejump/DJServlet.java
+++ trunk/server/src/tk/doodlejump/DJServlet.java
@@ -1,11 +1,55 @@
 package tk.doodlejump;
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.http.*;
+
+import com.xiaonei.api.XiaoneiException;
+import com.xiaonei.api.XiaoneiRestClient;
+import com.xiaonei.api.schema.Friend;
+import com.xiaonei.api.schema.FriendsGetFriendsResponse;
 
 @SuppressWarnings("serial")
 public class DJServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		
+		XiaoneiRestClient client = new XiaoneiRestClient("apiKey", "secret", "sessionKey"); 
+	    try {
+			client.friends_getFriends();
+		} catch (XiaoneiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	    FriendsGetFriendsResponse respa = (FriendsGetFriendsResponse) client.getResponsePOJO(); 
+	    List<Friend> friends = respa.getFriend(); 
+	    for (Friend friend : friends) { 
+	    	resp.getWriter().println("friends|getFriends|Id|"+friend.getId()); 
+	    	resp.getWriter().println("friends|getFriends|Name|"+friend.getName()); 
+	    	resp.getWriter().println("friends|getFriends|Headurl|"+friend.getHeadurl()); 
+	    } 
+		resp.getWriter().println();
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+		
+		XiaoneiRestClient client = new XiaoneiRestClient("apiKey", "secret", "sessionKey"); 
+	    try {
+			client.friends_getFriends();
+		} catch (XiaoneiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	    FriendsGetFriendsResponse respa = (FriendsGetFriendsResponse) client.getResponsePOJO(); 
+	    List<Friend> friends = respa.getFriend(); 
+	    for (Friend friend : friends) { 
+	    	resp.getWriter().println("friends|getFriends|Id|"+friend.getId()); 
+	    	resp.getWriter().println("friends|getFriends|Name|"+friend.getName()); 
+	    	resp.getWriter().println("friends|getFriends|Headurl|"+friend.getHeadurl()); 
+	    }  
+		resp.getWriter().println();
 	}
 }
--- trunk/server/war/index.jsp
+++ trunk/server/war/index.jsp
@@ -1,10 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    
-<div style="text-align: center; margin: 30px 0 30px 0;">
-    第一个示例::<a href="demo1.jsp" target="_blank">浏览器窗口的标题、显示使用者资料</a>
-    <br /><br />
-    第二个示例:<a href="demo2.jsp" target="_blank">邀请好友安装</a>
-    <br /><br />
-    第三个示例:<a href="demo3.jsp" target="_blank">通过好友邀请框的提交，获得好友ID号</a>
+<%@ page language="java" pageEncoding="UTF-8"%>
+
+<b>使用者的名称及其大中小三种头像：</b>
+<div style="text-align: center;">
+	图片：
+		<xn:profile-pic uid="loggedinuser" linked="true" size="tiny"/>
+		<xn:profile-pic uid="loggedinuser" linked="true" size="normal"/>
+		<xn:profile-pic uid="loggedinuser" linked="true" size="main"/>
+	<br />
+	姓名：
+		<xn:name uid="loggedinuser" linked="true" shownetwork="true" />
 </div>
